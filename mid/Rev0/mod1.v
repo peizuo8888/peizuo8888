126e37ef6eebf4afdb3d3ee21c23ac9d298a6cb4
peizuo8888 <peizuo1190@gmail.com>
2026-04-06 00:52:21 +0800

    update

--- mid/Rev0/mod1.v
+++ mid/Rev0/mod1.v
@@ -1,4 +1,13 @@
-module mod1(clk,clk_2hz,rst_n,button,out_0,out_1,out_2,out_3);
+module mod1 (
+           clk,
+           clk_2hz,
+           rst_n,
+           button,
+           out_0,
+           out_1,
+           out_2,
+           out_3
+       );
 input clk;
 input clk_2hz;
 input rst_n;
@@ -7,20 +16,21 @@ output reg [6:0] out_0;
 output reg [6:0] out_1;
 output reg [6:0] out_2;
 output reg [6:0] out_3;
-reg [2:0] num;
-reg [2:0] counter;
-reg [4:0] cr_state;
-reg [4:0] nt_state;
+reg  [2:0] num;
+reg  [2:0] counter;
+reg  [4:0] cr_state;
+reg  [4:0] nt_state;
 
 wire [6:0] seg_right;
 wire [6:0] seg_left;
 
 
-parameter       idle        =   5'b00001,
-                ex_right    =   5'b00010,
-                right       =   5'b00100,
-                ex_left     =   5'b01000,
-                left        =   5'b10000;
+parameter
+    idle        =   5'b00001,
+    ex_right    =   5'b00010,
+    right       =   5'b00100,
+    ex_left     =   5'b01000,
+    left        =   5'b10000;
 
 
 
@@ -40,10 +50,10 @@ begin
         case (cr_state)
             idle:
             begin
-                out_0 <= out_1;
-                out_1 <= out_2;
-                out_2 <= out_3;
-                out_3 <= 7'b111_1001;
+                out_0 <= out_0;
+                out_1 <= out_1;
+                out_2 <= out_2;
+                out_3 <= out_3;
             end
             ex_right:
             begin
@@ -79,16 +89,16 @@ end
 always @(*)
 begin
     case (cr_state)
-        idle    :
+        idle:
             nt_state <= right;
         ex_right:
-            nt_state <= (counter >= num-1) ? right: ex_right;
-        right   :
-            nt_state <= (counter == 3'd3) ? ((num==1)? left : ex_left ): right;
-        ex_left :
-            nt_state <= (counter >= num-1) ? left: ex_left;
-        left    :
-            nt_state <= (counter == 3'd3) ? ((num == 1)? right:ex_right): left;
+            nt_state <= (counter >= num - 1) ? right : ex_right;
+        right:
+            nt_state <= (counter == 3'd4) ? ex_left : right;
+        ex_left:
+            nt_state <= (counter >= num - 1) ? left : ex_left;
+        left:
+            nt_state <= (counter == 3'd4) ? ex_right : left;
     endcase
 end
 
@@ -96,13 +106,13 @@ always @(posedge clk or negedge rst_n)
 begin
     if (!rst_n)
     begin
-        num <= 3'b1;
+        num <= 3'd2;
     end
     else
     begin
         if (button)
         begin
-            num<= num+1'b1;
+            num <= num + 1'b1;
         end
         else
         begin
@@ -120,7 +130,7 @@ begin
     begin
         if (cr_state != nt_state)
         begin
-            counter<= 3'b1;
+            counter <= 3'b1;
         end
         else
         begin
@@ -128,6 +138,16 @@ begin
         end
     end
 end
-seven_seg M11(.clk(clk), .rst_n(rst_n), .data(counter+1), .o(seg_right));
-seven_seg M12(.clk(clk), .rst_n(rst_n), .data(num-counter), .o(seg_left));
+seven_seg M11 (
+              .clk(clk),
+              .rst_n(rst_n),
+              .data(counter),
+              .o(seg_right)
+          );
+seven_seg M12 (
+              .clk(clk),
+              .rst_n(rst_n),
+              .data(num - counter),
+              .o(seg_left)
+          );
 endmodule
