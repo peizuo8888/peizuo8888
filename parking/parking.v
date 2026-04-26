b6b20d40cc674bdb3916d49320398016f168ac93
peizuo8888 <peizuo1190@gmail.com>
2026-04-27 05:03:37 +0800

    Update parking.v

--- parking/parking.v
+++ parking/parking.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/04/27 03:31:48
+// Design Name: 
+// Module Name: parking
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module parking (
     input clk,
     input rst_n,
@@ -9,10 +31,11 @@ module parking (
     output [7:0] sram_addr,
     output [7:0] sram_din,
     output reg [7:0] parking_space,
-    output [2:0] state_dbg 
+    output [2:0] state_dbg, 
+    output reg [2:0] counter
 );
 parameter ADDRESS = 8'h00;
-reg [2:0] counter;
+//reg [2:0] counter;
 reg [2:0] cr_state;
 reg [2:0] nt_state;
 reg car_in_reg;
@@ -25,27 +48,25 @@ parameter           IDLE          = 3'd0,
                     WRITE_SRAM    = 3'd3,
                     DONE          = 3'd4;
 
-always @(posedge clk ) begin
+always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         cr_state        <= IDLE;     
         counter         <= 3'b0;
         car_in_reg      <= 1'b0;          
         car_out_reg     <= 1'b0;
-        parking_space   <= 8'b0;        
+        parking_space   <= 8'd8;        
     end else begin
         cr_state <= nt_state;
         case (cr_state)
             IDLE    :begin
                     if (car_in || car_out) begin
                         counter <= counter + 1'b1;
-                    end else if (counter == 3'd3)begin
-                        car_in_reg  <= car_in;         
-                        car_out_reg <= car_out;         
-                    end
+                    end else counter <= 3'b0;
+                    car_in_reg  <= car_in;         
+                    car_out_reg <= car_out;         
             end
             READ_SRAM:begin 
-                    parking_space <= sram_din;
-                    counter <= 3'b0;
+                    parking_space <= sram_dout;
             end
             UPDATE   :begin
                 if (car_in_reg) begin
@@ -55,7 +76,7 @@ always @(posedge clk ) begin
                 end
             end
             WRITE_SRAM:;
-            DONE:;
+            DONE:counter <= 3'b0;
         endcase
     end
 end
@@ -63,12 +84,12 @@ end
 
 always @(*) begin
     case (cr_state)
-        IDLE        :   nt_state = (counter     == 3'd3) ? READ_SRAM : IDLE;
+        IDLE        :   nt_state = (counter     == 3'd2) ? READ_SRAM : IDLE;
         default:        nt_state = (cr_state    == 3'd4) ? 3'b0 : cr_state + 1'b1; 
     endcase
 end
 assign sram_we      = (cr_state == WRITE_SRAM) ? 1'b1 : 1'b0;  
-assign sram_dout    = parking_space;
+assign sram_din     = parking_space;
 assign sram_addr    = ADDRESS;
 assign state_dbg    = cr_state;
 assign sram_en      = 1'b1;
